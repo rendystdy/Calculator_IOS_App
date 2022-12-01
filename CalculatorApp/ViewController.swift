71cdc0ccbce3e989d3a5b89727e67a9cfb94cfa7
Rendy Setiadi <rendysetiady8@gmail.com>
2022-12-01 21:20:57 +0700

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Rendy Setiadi on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
